a32e3f12a33a34c71cd9ce2b50fe6786f6a9edf8
lizardthunder <lizardthunder@protonmail.com>
2016-02-07 22:09:26 -0500

    heavily updated wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -6,6 +6,7 @@
 
 ########### defaults
 verbose=false
+url="www.google.com" # url for test pinging
 
 ########### helper functions
 
@@ -21,42 +22,86 @@ function getinterface() {
     fi
 }
 
-########### functions
-function scan() {
-    getinterface
-    $verbose && iw dev $interface scan | grep -e "$interface" -e "signal:" -e "SSID" -e "RSN" -e "WPA" -e "WPS" -e "Privacy:"
-    ! $verbose && iw dev $interface scan | grep -e "SSID" | sed s/[[:space:]]*SSID:[[:space:]]//
+function checknetwork() {
+    if $verbose; then
+        ping $url -c 1 &>/dev/null && echo "Connected to $1 successfully!" || echo "Did not connect to $1!" && exit 1
+    else
+        dhcpcd $interface &>/dev/null
+        ping $url -c 1 &>/dev/null || exit 1
+    fi
 }
 
-#function disconnect() {
-#    
-#}
+########### functions
 
-#function connect() {
-#
-#}
+# scans for and outputs the SSIDs of available wifi networks
+# with verbose mode, also indicates BSS identifier, signal strength, and security measures
+function scan() {
+    getinterface
+    if $verbose; then
+        echo "Scanning for networks..." && \
+            iw dev $interface scan | grep -e "$interface" -e "signal:" -e "SSID" -e "RSN" -e "WPA" -e "WPS" -e "Privacy:" || \
+                echo "No networks found!" && exit 1
+    else
+        iw dev $interface scan | grep -e "SSID" | sed s/[[:space:]]*SSID:[[:space:]]// || \
+            exit 1
+    fi
+}
+
+# kills all running instances of wpa_supplicant and iw dev
+function disconnect() {
+    pkill wpa_supplicant || exit 1
+    pkill "iw dev" || exit 1
+    echo "Successfully disconnected!"
+}
+
+# takes an SSID, then connects to it using $interface
+# automatically uses highest level of security
+function connect() {
+    getinterface
+    #if $1 is WPA or WPA2; then
+        #read -s -p "Password: " password
+        #echo
+        #$verbose && echo "Connecting to $1..."
+        #wpa_supplicant -D nl80211,wext -i $interface -c <(wpa_passphrase "$1" "$password") &> /dev/null &
+        #password="" # for extra security, i guess
+        #sleep 7 # arbitrary time, have noticed that dhcpcd doesn't stick unless you wait a bit
+        #$verbose && dhcpcd $interface || dhcpcd $interface &>/dev/null
+    #else
+        #if $1 is WEP encrypted; then
+            #read -s -p "Password: " password
+            #echo
+            #iw dev $interface connect "$1" key 0:$password &
+            #password="" # again, i guess for more security?
+        #else
+            #iw dev $interface connect "$1" &
+        #fi
+    #fi
+    #checknetwork $1
+}
 
 ########### parse options
 
 function usage() {
     cat <<EOF
-usage: $(basename $0) [-v] [-hSD] [-C wifi_name]
-       -S             scan for available wifi networks and exit
-       -D             disconnect from all wifi networks and exit
-       -C wifi_name   connect to network named wifi_name and exit
-       -h             display this message and exit
-       -v             activate verbose mode
-                      without verbose mode, will silently error out
+usage: $(basename $0) [-v] [-hSD] [-u url] [-C wifi_name]
+         -S         scan for available wifi networks, print their SSIDs, and exit
+         -D         disconnect from all wifi networks and exit
+         -u url     specify url for test pinging when connecting
+         -C ssid    connect to network with an SSID of ssid and exit
+         -h         display this message and exit
+         -v         activate verbose mode
+                     * without verbose mode, will silently error
 EOF
 }
 
-while getopts "vhSDC:" opt; do
+while getopts "vhSDC:u:" opt; do
     case $opt in
         v) verbose=true ;;
         h) usage; exit 0 ;;
         S) scan; exit 0 ;;
         D) disconnect; exit 0 ;;
         C) connect "$OPTARG" ; exit 0 ;;
+        u) url="$OPTARG" ;;
         ?) usage; exit 1 ;;
     esac
 done
